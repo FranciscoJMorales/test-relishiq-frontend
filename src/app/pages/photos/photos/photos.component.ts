576e10f7549f96b40427ac4fd3c6ca00e6a7b5a0
FranciscoJMorales <morales.francisco0206@gmail.com>
2024-06-05 16:15:39 -0600

    feat: get filter data from query

--- src/app/pages/photos/photos/photos.component.ts
+++ src/app/pages/photos/photos/photos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Photo } from '../../../interfaces/photo';
 import { PhotosService } from '../../../services/photos.service';
@@ -18,12 +19,18 @@ export class PhotosComponent implements OnInit {
   loading: boolean = true;
 
   filters: FormGroup;
+
+  title: string | null = null;
+  albumTitle: string | null = null;
+  albumUserEmail: string | null = null;
   offset: number = 0;
   limit: number = 25;
 
   photos: Array<Photo> = [];
 
   constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute, 
     private formBuilder: FormBuilder,
     private photosService: PhotosService,
   ) {
@@ -35,18 +42,62 @@ export class PhotosComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // Get values from query string
+    this.title = this.activatedRoute.snapshot.queryParams['title'] ?? null;
+    this.albumTitle = this.activatedRoute.snapshot.queryParams['album.title'] ?? null;
+    this.albumUserEmail = this.activatedRoute.snapshot.queryParams['album.user.email'] ?? null;
+    this.offset = this.activatedRoute.snapshot.queryParams['offset'] ?? 0;
+    this.limit = this.activatedRoute.snapshot.queryParams['limit'] ?? 25;
+
+    this.filters.reset({
+      title: this.title,
+      albumTitle: this.albumTitle,
+      albumUserEmail: this.albumUserEmail,
+    });
+
     this.getPhotos();
   }
 
-  async getPhotos() {
+  async getPhotos(): Promise<void> {
     this.loading = true;
     try {
-      const { title, albumTitle, albumUserEmail } = this.filters.value;
-      this.photos = await this.photosService.getPhotos(title, albumTitle, albumUserEmail, this.offset, this.limit);
+      this.router.navigate(
+        [], 
+        {
+          relativeTo: this.activatedRoute,
+          queryParams: {
+            title: this.title,
+            "album.title": this.albumTitle,
+            "album.user.email": this.albumUserEmail,
+            offset: this.offset !== 0 ? this.offset : null,
+            limit: this.limit !== 25 ? this.limit : null,
+          },
+        }
+      );
+
+      this.photos = await this.photosService.getPhotos(this.title, this.albumTitle, this.albumUserEmail, this.offset, this.limit);
     }
     catch {
       this.photos = [];
     }
     this.loading = false;
+  }
+
+  applyFilters(): void {
+    const { title, albumTitle, albumUserEmail } = this.filters.value;
+    this.title = title;
+    this.albumTitle = albumTitle;
+    this.albumUserEmail = albumUserEmail;
+    this.offset = 0;
+    this.getPhotos();
+  }
+
+  clearFilters(): void {
+    this.filters.reset();
+    this.title = null;
+    this.albumTitle = null;
+    this.albumUserEmail = null;
+    this.offset = 0;
+    this.getPhotos();
   }
 }
